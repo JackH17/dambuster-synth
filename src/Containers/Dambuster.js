af40e102d58eb91fa379a36708f985bd6450bc2f
JackH17 <jack.hawkins77@googlemail.com>
2020-01-26 17:39:54 +0000

    initial setup

---
+++ src/Containers/Dambuster.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react'
+
+const Dambuster = () => {
+
+    const [audioCTX, setAudioCTX] = useState(false);
+    const [userContext, setUserContext] = useState();
+
+    const cBluesScale = [
+        {
+            note: 'C4',
+            frequency: 261.63
+        },
+        {
+            note: 'Eb4',
+            frequency: 311.13
+        },
+        {
+            note: 'F4',
+            frequency: 349.23
+        },
+        {
+            note: 'Gb4',
+            frequency: 369.99
+        },
+        {
+            note: 'G4',
+            frequency: 392.00
+        },
+        {
+            note: 'Bb4',
+            frequency: 466.16
+        },
+        {
+            note: 'C5',
+            frequency: 523.25
+        },
+    ]
+
+    const [input, setInput] = useState();
+    const [masterGain, setMaterGain] = useState();
+    const [output, setOutput] = useState();
+
+    const [oscillators, setOscillators] = useState([]);
+
+
+
+    const createAudioNodes = (userContext) => {
+        setInput(userContext);
+        setMaterGain(userContext.createGain())
+        setOutput(userContext.destination);
+    }
+
+    useEffect(() => {
+        if(!userContext){
+            return;
+        }
+        createAudioNodes(userContext);
+    }, [userContext])
+
+    const getContext = async () => {
+
+        const AudioContext = await window.AudioContext || window.webkitAudioContext;
+        const context =  await new AudioContext();
+
+        if(!context){
+            window.alert('Current browser not supported')
+            return;
+        }
+
+        setUserContext(context)
+        setAudioCTX(!audioCTX)  
+    };
+
+    const playTone = async (e) => {
+
+
+        const nodeId = e.target.id;
+        const frequency = e.target.value
+
+        const oscNode = await userContext.createOscillator();
+        oscNode.type = 'sine';
+        oscNode.frequency.value = frequency;
+
+        const oscObj = {
+            node: oscNode,
+            id: nodeId
+        };
+
+        setOscillators([...oscillators, oscObj])
+        
+        oscNode.connect(masterGain);
+        masterGain.connect(output);
+
+        oscNode.start();
+    };
+
+    const stopTone = async (e) => {
+
+        const oscObj = await oscillators.filter(osc => osc.id === e.target.id);
+
+        const now = userContext.currentTime;
+
+        oscObj[0].node.stop(now);
+        oscObj[0].node.disconnect(masterGain);
+        oscillators.splice(oscObj[0]);
+    }
+
+
+    return (
+        <div>
+            <h1>Dambuster</h1>
+            {!audioCTX && <button onClick={getContext}>get audio</button>}
+            {audioCTX && cBluesScale.map(note => <button key={note.note} id={note.note} value={note.frequency} onMouseDown={playTone} onMouseUp={stopTone}> {note.note.slice(0, -1)}</button>)}
+        </div>
+    )
+};
+
+export default Dambuster;
