c08371191918406a2b56fd24c0184194471408de
JackH17 <jack.hawkins77@googlemail.com>
2020-01-28 14:37:40 +0000

    initial u-i integrated

--- src/Containers/Dambuster.js
+++ src/Containers/Dambuster.js
@@ -1,41 +1,70 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { resolve } from 'dns';
+import Konva from 'konva';
+import { Stage, Layer, Rect, Circle, Text } from "react-konva";
+import { octaves } from '../utils/NoteFrequencies';
 
 const Dambuster = () => {
 
     const [audioCTX, setAudioCTX] = useState(false);
     const [userContext, setUserContext] = useState();
 
-    const cBluesScale = [
-        {
-            note: 'C4',
-            frequency: 261.63
-        },
-        {
-            note: 'Eb4',
-            frequency: 311.13
-        },
-        {
-            note: 'F4',
-            frequency: 349.23
-        },
-        {
-            note: 'Gb4',
-            frequency: 369.99
-        },
-        {
-            note: 'G4',
-            frequency: 392.00
-        },
-        {
-            note: 'Bb4',
-            frequency: 466.16
-        },
-        {
-            note: 'C5',
-            frequency: 523.25
-        },
-    ]
+    const [stageWidth, setStageWidth] = useState(window.innerWidth);
+    const [stageHeight, setStageHeight] = useState(window.innerHeight);
+
+    const [octaveSet, setOctaveSet] = useState(4);
+    const currentOctave = useRef();
+
+    const [cPressed, setCPressed] = useState(false);
+    const [dPressed, setDPressed] = useState(false);
+    const [ePressed, setEPressed] = useState(false);
+    const [fPressed, setFPressed] = useState(false);
+    const [gPressed, setGPressed] = useState(false);
+    const [aPressed, setAPressed] = useState(false);
+    const [bPressed, setBPressed] = useState(false);
+
+    const [cSharpPressed, setCSharpPressed] = useState(false);
+    const [eFlatPressed, setEFlatPressed] = useState(false);
+    const [fSharpPressed, setFSharpPressed] = useState(false);
+    const [gSharpPressed, setGSharpPressed] = useState(false);
+    const [bFlatPressed, setBFlatPressed] = useState(false);
+
+    const [octaveSelected, setOctaveSelected] = useState(false)
+
+    const stageRef = useRef();
+
+    const updateWidthAndHeight = () => {
+
+        setStageWidth(window.innerWidth);
+        setStageHeight(window.innerHeight);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWidthAndHeight)
+
+        return () => {
+            window.removeEventListener('resize', updateWidthAndHeight);
+        }
+    });
+
+    const widthPercentage = (width) => {   
+        return Math.floor(stageWidth / (100 / width))
+    }
+
+    const heightPercentage = (height) => {   
+        return Math.floor(stageHeight / (100 / height))
+    }
+
+    useEffect(() => {
+
+        setOctaveSelected(false);
+
+        if(octaveSet === 4){
+            currentOctave.current = octaves.fourth
+        }
+
+        setOctaveSelected(true)
+
+    }, [octaveSet])
 
     const waveforms = ['sine', 'square', 'sawtooth', 'triangle'];
 
@@ -46,69 +75,159 @@ const Dambuster = () => {
     const [output, setOutput] = useState();
 
     const noteOne = useRef();
+    const noteTwo = useRef();
+    const noteThree = useRef();
+    const noteFour = useRef();
+    const noteFive = useRef();
+    const noteSix = useRef();
+    const noteSeven = useRef();
+
+    const sharpOne = useRef();
+    const sharpTwo = useRef();
+    const sharpThree = useRef();
+    const sharpFour = useRef();
+    const sharpFive = useRef();
+
 
     const [oscillators, setOscillators] = useState([]);
 
     const onDown = (e) => {
 
-        if(e.key === `a`){
-            playTone('C4', 261.63)
-        };
-
-        if(e.key === `s`){
-            console.log(`s pressed`)
-        };
+        if(e.repeat) {
+            return;
+        }
 
         if(e.key === `d`){
-            console.log(`d pressed`)
+            setCPressed(!cPressed)
+            playTone(noteOne.current.attrs)
+        };
+
+        if(e.key === `r`){
+            setCSharpPressed(!cSharpPressed)
+            playTone(sharpOne.current.attrs)
+            
         };
 
         if(e.key === `f`){
-            console.log(`f pressed`)
+            setDPressed(!dPressed)
+            playTone(noteTwo.current.attrs)
+            
         };
 
+        if(e.key === `t`){
+            setEFlatPressed(!eFlatPressed)
+            playTone(sharpTwo.current.attrs)
+        };
+
+
         if(e.key === `g`){
-            console.log(`g pressed`)
+            setEPressed(!ePressed)
+            playTone(noteThree.current.attrs)
+            
         };
 
         if(e.key === `h`){
-            console.log(`h pressed`)
+            setFPressed(!fPressed)
+            playTone(noteFour.current.attrs)
+            
+        };
+
+        if(e.key === `u`){
+            setFSharpPressed(!fSharpPressed)
+            playTone(sharpThree.current.attrs)
         };
 
         if(e.key === `j`){
-            console.log(`j pressed`)
+            setGPressed(!gPressed)
+            playTone(noteFive.current.attrs)
         };
+
+        if(e.key === `i`){
+            setGSharpPressed(!gSharpPressed)
+            playTone(sharpFour.current.attrs)
+        };
+
+        if(e.key === `k`){
+            setAPressed(!aPressed)
+            playTone(noteSix.current.attrs)
+        };
+
+        if(e.key === `o`){
+            setBFlatPressed(!bFlatPressed)
+            playTone(sharpFive.current.attrs)
+        };
+
+        if(e.key === `l`){
+            setBPressed(!bPressed)
+            playTone(noteSeven.current.attrs)
+        };
+
     }
 
     const onUp = (e) => {
-        
-        if(e.key === `a`){
-            console.log('a up')
-        };
-
-        if(e.key === `s`){
-            console.log(`s up`)
-        };
 
         if(e.key === `d`){
-            console.log(`d up`)
+            setCPressed(!cPressed)
+            stopTone(noteOne.current.attrs)
+        };
+
+        if(e.key === `r`){
+            setCSharpPressed(!cSharpPressed)
+            stopTone(sharpOne.current.attrs)
         };
 
         if(e.key === `f`){
-            console.log(`f up`)
+            setDPressed(!dPressed)
+            stopTone(noteTwo.current.attrs)
         };
 
+        if(e.key === `t`){
+            setEFlatPressed(!eFlatPressed)
+            stopTone(sharpTwo.current.attrs)
+        };
+
+
         if(e.key === `g`){
-            console.log(`g up`)
+            setEPressed(!ePressed)
+            stopTone(noteThree.current.attrs)
         };
 
         if(e.key === `h`){
-            console.log(`h up`)
+            setFPressed(!fPressed)
+            stopTone(noteFour.current.attrs)
+        };
+
+        if(e.key === `u`){
+            setFSharpPressed(!fSharpPressed)
+            stopTone(sharpThree.current.attrs)
         };
 
         if(e.key === `j`){
-            console.log(`j up`)
+            setGPressed(!gPressed)
+            stopTone(noteFive.current.attrs)
         };
+
+        if(e.key === `i`){
+            setGSharpPressed(!gSharpPressed)
+            stopTone(sharpFour.current.attrs)
+        };
+
+        if(e.key === `k`){
+            setAPressed(!aPressed)
+            stopTone(noteSix.current.attrs)
+        };
+
+        if(e.key === `o`){
+            setBFlatPressed(!bFlatPressed)
+            stopTone(sharpFive.current.attrs)
+        };
+
+        if(e.key === `l`){
+            setBPressed(!bPressed)
+            stopTone(noteSeven.current.attrs)
+        };
+
+
     }
 
     useEffect(() => {
@@ -128,8 +247,6 @@ const Dambuster = () => {
         const gain = await userContext.createGain();
         const output = await userContext.destination;
 
-        noteOne.current = userContext.createGain()
-
         gain.connect(output)
         setMaterGain(gain);
         setOutput(output);
@@ -143,6 +260,7 @@ const Dambuster = () => {
         if(!userContext){
             return;
         }
+        console.log('creating nodes')
         createAudioNodes(userContext);
     }, [userContext])
 
@@ -160,10 +278,10 @@ const Dambuster = () => {
         setAudioCTX(!audioCTX)  
     };
 
-    const playTone = async (id, freq) => {
+    const playTone = async ({id, value}) => {
 
         const nodeId = id;
-        const frequency = freq;
+        const frequency = value;
 
         const oscNode = await userContext.createOscillator();
         oscNode.type = oscillatorWave;
@@ -179,7 +297,7 @@ const Dambuster = () => {
         };
 
         const now = userContext.currentTime;
-        const attack = now + 0.2;
+        const attack = now + 0.001;
 
         setOscillators([...oscillators, oscObj])
         
@@ -191,9 +309,10 @@ const Dambuster = () => {
     };
 
 
-    const stopTone = async (e) => {
+    const stopTone = async ({id}) => {
 
-        const oscObj = await oscillators.filter(osc => osc.id === e.target.id);
+
+        const oscObj = await oscillators.filter(osc => osc.id === id);
 
         const now = userContext.currentTime;
         const release = now + 0.2;
@@ -209,25 +328,51 @@ const Dambuster = () => {
         setOscillatorWave(e.target.value)
     };
 
+
     return (
         <div>
-            <h1>Dambuster</h1>
-            <div >
-                {!audioCTX && <button onClick={getContext}>get audio</button>}
-            </div>
-            <div>
-                <select onChange={handleWaveformChange}>
-                    {waveforms.map(wave => <option value={wave}>{wave}</option>)}
-                </select>
-            </div>
-
-            <div>
-                {audioCTX && cBluesScale.map(note => 
-                <button key={note.note} id={note.note} value={note.frequency}
-                 onMouseDown={playTone} onMouseUp={stopTone}> {note.note.slice(0, -1)}</button>)}
-            </div>
+            <Stage ref={stageRef} width={stageWidth} height={stageHeight}>
+                <Layer>
+                    
+                    <Rect width={stageWidth} height={stageHeight} fill='pink'/>
+                    <Circle x={widthPercentage(5)} y={heightPercentage(5)} radius={widthPercentage(5)} fill='white' onClick={getContext}/>
+                    <Rect ref={noteOne} id={octaveSelected && currentOctave.current ? currentOctave.current.one.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.one.frequency : null}  x={widthPercentage(30)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={cPressed ? 'yellow' : 'white'}/>
+                    <Rect ref={noteTwo}  id={octaveSelected && currentOctave.current ? currentOctave.current.two.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.two.frequency : null} x={widthPercentage(35)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={dPressed ? 'yellow' : 'white'}/>
+                    <Rect ref={noteThree}  id={octaveSelected && currentOctave.current ? currentOctave.current.three.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.three.frequency : null} x={widthPercentage(40)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={ePressed ? 'yellow' : 'white'}/>
+                    <Rect ref={noteFour}  id={octaveSelected && currentOctave.current ? currentOctave.current.four.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.four.frequency : null} x={widthPercentage(45)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={fPressed ? 'yellow' : 'white'}/>
+                    <Rect ref={noteFive}  id={octaveSelected && currentOctave.current ? currentOctave.current.five.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.five.frequency : null} x={widthPercentage(50)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={gPressed ? 'yellow' : 'white'}/>
+                    <Rect ref={noteSix}  id={octaveSelected && currentOctave.current ? currentOctave.current.six.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.six.frequency : null} x={widthPercentage(55)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={aPressed ? 'yellow' : 'white'}/>
+                    <Rect ref={noteSeven}  id={octaveSelected && currentOctave.current ? currentOctave.current.seven.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.seven.frequency : null} x={widthPercentage(60)} y={heightPercentage(70)} width={widthPercentage(3)} height={heightPercentage(10)} fill={bPressed ? 'yellow' : 'white'}/>
+            
+                    <Rect ref={sharpOne}  id={octaveSelected && currentOctave.current ? currentOctave.current.sharpOne.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.sharpOne.frequency : null} x={widthPercentage(32.5)} y={heightPercentage(60)} width={widthPercentage(3)} height={heightPercentage(10)} fill={cSharpPressed ? 'yellow' : 'black'}/>
+                    <Rect ref={sharpTwo}  id={octaveSelected && currentOctave.current ? currentOctave.current.sharpTwo.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.sharpTwo.frequency : null} x={widthPercentage(37.5)} y={heightPercentage(60)} width={widthPercentage(3)} height={heightPercentage(10)} fill={eFlatPressed ? 'yellow' : 'black'}/>
+    
+                    <Rect ref={sharpThree}  id={octaveSelected && currentOctave.current ? currentOctave.current.sharpThree.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.sharpThree.frequency : null} x={widthPercentage(47.5)} y={heightPercentage(60)} width={widthPercentage(3)} height={heightPercentage(10)} fill={fSharpPressed ? 'yellow' : 'black'}/>
+                    <Rect ref={sharpFour}  id={octaveSelected && currentOctave.current ? currentOctave.current.sharpFour.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.sharpFour.frequency : null} x={widthPercentage(52.5)} y={heightPercentage(60)} width={widthPercentage(3)} height={heightPercentage(10)} fill={gSharpPressed ? 'yellow' : 'black'}/>
+                    <Rect ref={sharpFive}  id={octaveSelected && currentOctave.current ? currentOctave.current.sharpFive.note : null} value={octaveSelected && currentOctave.current ? currentOctave.current.sharpFive.frequency : null} x={widthPercentage(57.5)} y={heightPercentage(60)} width={widthPercentage(3)} height={heightPercentage(10)} fill={bFlatPressed ? 'yellow' : 'black'}/>
+                </ Layer>
+            </ Stage>
 
         </div>
+        
+        // <div>
+        //     <h1>Dambuster</h1>
+        //     <div >
+        //         {!audioCTX && <button onClick={getContext}>get audio</button>}
+        //     </div>
+        //     <div>
+        //         <select onChange={handleWaveformChange}>
+        //             {waveforms.map(wave => <option value={wave}>{wave}</option>)}
+        //         </select>
+        //     </div>
+
+        //     <div>
+        //         {audioCTX && cBluesScale.map(note => 
+        //         <button key={note.note} id={note.note} value={note.frequency}
+        //          onMouseDown={playTone} onMouseUp={stopTone}> {note.note.slice(0, -1)}</button>)}
+        //     </div>
+
+        // </div>
     )
 };
 
---
+++ src/utils/NoteFrequencies.js
@@ -0,0 +1,53 @@
+export const octaves = {
+    fourth : {
+
+        one: {
+            note: 'C4',
+            frequency: 261.63
+        },
+        two: {
+            note: 'D4',
+            frequency: 293.66
+        },
+        three: {
+            note: 'E4',
+            frequency: 329.63
+        },
+        four: {
+            note: 'F4',
+            frequency: 349.23
+        },
+        five: {
+            note: 'G4',
+            frequency: 392.00
+        },
+        six: {
+            note: 'A4',
+            frequency: 440.00
+        },
+        seven: {
+            note: 'B4',
+            frequency: 493.88
+        },
+        sharpOne: {
+            note: 'C#4',
+            frequency: 277.18
+        },
+        sharpTwo: {
+            note: 'Eb4',
+            frequency: 311.13
+        },
+        sharpThree: {
+            note: 'F#4',
+            frequency: 369.99
+        },
+        sharpFour: {
+            note: 'G#4',
+            frequency: 415.30
+        },
+        sharpFive: {
+            note: 'Bb4',
+            frequency: 466.16
+        }
+    }
+};
